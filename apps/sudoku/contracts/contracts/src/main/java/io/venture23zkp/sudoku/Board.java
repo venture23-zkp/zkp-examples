a835b821385ac58738d22d2d803d7f02d356ca53
bisbist <bistbishal69@gmail.com>
2023-04-21 09:40:30 +0545

    Update contracts for getting random board

--- apps/sudoku/contracts/contracts/src/main/java/io/venture23zkp/sudoku/Board.java
+++ apps/sudoku/contracts/contracts/src/main/java/io/venture23zkp/sudoku/Board.java
@@ -1,17 +1,29 @@
 package io.venture23zkp.sudoku;
 
-import score.Context;
+import java.math.BigInteger;
+
 import score.ObjectReader;
 import score.ObjectWriter;
 
 public class Board {
 
+    private final BigInteger sha256Id;
+    private final BigInteger pedersenId;
     private final int[][] data;
 
-    Board(int[][] data) {
+    Board(BigInteger sha256Id, BigInteger pedersenId, int[][] data) {
+        this.sha256Id = sha256Id;
+        this.pedersenId = pedersenId;
         this.data = data;
     }
 
+    public BigInteger getSha256Id() {
+        return sha256Id;
+    }
+
+    public BigInteger getPedersenId() {
+        return pedersenId;
+    }
 
     public int[][] getData() {
         return this.data;
@@ -22,7 +34,9 @@ public class Board {
     }
 
     public void writeObject(ObjectWriter w) {
-        w.beginList(81);
+        w.beginList(81+2);
+        w.write(sha256Id);
+        w.write(pedersenId);
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
                 w.write(this.data[i][j]);
@@ -33,6 +47,8 @@ public class Board {
 
     public static Board readObject(ObjectReader r) {
         r.beginList();
+        BigInteger sha256Id = r.readBigInteger();
+        BigInteger pedersenId = r.readBigInteger();
         int[][] data = new int[9][9];
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
@@ -40,6 +56,6 @@ public class Board {
             }
         }
         r.end();
-        return new Board(data);
+        return new Board(sha256Id, pedersenId, data);
     }
 }
--- apps/sudoku/contracts/contracts/src/main/java/io/venture23zkp/sudoku/Sudoku.java
+++ apps/sudoku/contracts/contracts/src/main/java/io/venture23zkp/sudoku/Sudoku.java
@@ -2,29 +2,26 @@
 package io.venture23zkp.sudoku;
 
 import java.math.BigInteger;
-import java.util.Map;
-import scorex.util.HashMap;
 
 import score.ArrayDB;
 import score.Context;
-import score.DictDB;
 import score.annotation.External;
 
 public class Sudoku {
 
-    private final Pedersen_Verifier pedersen_verifier;
-    private final Sha256_Verifier sha_verifier;
+    private final PedersenBN128Verifier pedersenBN128Verifier;
+    private final Sha256BLS12381Verifier sha256BLS12381Verifier;
 
+    private final ArrayDB<Board> boards = Context.newArrayDB("boards", Board.class);
 
-    private final DictDB<BigInteger, Board> boards = Context.newDictDB("boards", Board.class);
-    public final ArrayDB<BigInteger> boardIds = Context.newArrayDB("boardIds", BigInteger.class);
 
     public Sudoku() {
-        this.pedersen_verifier = new Pedersen_Verifier();
-        this.sha_verifier = new Sha256_Verifier();
+        this.pedersenBN128Verifier = new PedersenBN128Verifier();
+        this.sha256BLS12381Verifier = new Sha256BLS12381Verifier();
 
-        addBoard(
+        boards.add(new Board(
                 new BigInteger("12946702913587076100588339357837874244737833722059782620840121604018902625880"),
+                new BigInteger("20508280487233337829117787098579484117180647220128235675540805714795266829330"),
                 new int[][] {
                         { 1, 2, 7, 5, 8, 4, 6, 9, 3 },
                         { 8, 5, 6, 3, 7, 9, 1, 2, 4 },
@@ -35,10 +32,12 @@ public class Sudoku {
                         { 5, 8, 3, 2, 9, 7, 4, 6, 1 },
                         { 7, 1, 4, 8, 3, 6, 9, 5, 2 },
                         { 6, 9, 2, 1, 4, 5, 3, 0, 7 }
-                });
+                }
+        ));
 
-        addBoard(
+        boards.add(new Board(
                 new BigInteger("107892420655660906236019058477082710171022090417415360920355362860546940302805"),
+                new BigInteger("4734607875457860383511038714947529190779064469715356965082230943676408289143"),
                 new int[][] {
                         { 0, 2, 7, 5, 0, 4, 0, 0, 0 },
                         { 0, 0, 0, 3, 7, 0, 0, 0, 4 },
@@ -49,50 +48,45 @@ public class Sudoku {
                         { 0, 8, 3, 0, 9, 0, 4, 0, 0 },
                         { 7, 1, 0, 0, 0, 0, 9, 0, 2 },
                         { 0, 0, 0, 0, 0, 5, 0, 0, 7 }
-                });
+                }
+        ));
 
-        addBoard(
-                new BigInteger("50213289574608008120354010172600564686142848648930442262797942174114227273539"),
-                new int[][] {
-                        { 0, 0, 0, 0, 0, 6, 0, 0, 0 },
-                        { 0, 0, 7, 2, 0, 0, 8, 0, 0 },
-                        { 9, 0, 6, 8, 0, 0, 0, 1, 0 },
-                        { 3, 0, 0, 7, 0, 0, 0, 2, 9 },
-                        { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-                        { 4, 0, 0, 5, 0, 0, 0, 7, 0 },
-                        { 6, 5, 0, 1, 0, 0, 0, 0, 0 },
-                        { 8, 0, 1, 0, 5, 0, 3, 0, 0 },
-                        { 7, 9, 2, 0, 0, 0, 0, 0, 4 }
-                });
+        boards.add(new Board(
+            new BigInteger("50213289574608008120354010172600564686142848648930442262797942174114227273539"),
+            new BigInteger("8152119989451001230856454209031619574707505319220427314377706000948347561614"),
+            new int[][] {
+                    { 0, 0, 0, 0, 0, 6, 0, 0, 0 },
+                    { 0, 0, 7, 2, 0, 0, 8, 0, 0 },
+                    { 9, 0, 6, 8, 0, 0, 0, 1, 0 },
+                    { 3, 0, 0, 7, 0, 0, 0, 2, 9 },
+                    { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                    { 4, 0, 0, 5, 0, 0, 0, 7, 0 },
+                    { 6, 5, 0, 1, 0, 0, 0, 0, 0 },
+                    { 8, 0, 1, 0, 5, 0, 3, 0, 0 },
+                    { 7, 9, 2, 0, 0, 0, 0, 0, 4 }
+            }
+        ));
 
-    }
-
-    private void addBoard(BigInteger id, int[][] data) {
-        boardIds.add(id);
-        boards.set(id, new Board(data));
-    }
-
-    @External(readonly = true)
-    public BigInteger getRandomBoardId() {
-        int i = (int) (Context.getBlockTimestamp() % boardIds.size());
-        return boardIds.get(i);
     }
 
     @External(readonly = true)
-    public int[][] getBoardData(BigInteger id) {
-        return boards.get(id).getData();
+    public Board getRandomBoard() {
+        int i = (int) (Context.getBlockTimestamp() % boards.size());
+        return boards.get(i);
     }
 
     @External(readonly = true)
-    public boolean verify(BigInteger boardId, BigInteger[] a, BigInteger[][] b, BigInteger[] c, String verificationType) {
-        Context.require(boards.get(boardId) != null, "Board does not exist!");
-        BigInteger[] input = new BigInteger[] { boardId };
-
-        if (verificationType.equals("sha")) {
-            return this.sha_verifier.verifyProof(a, b, c, input);
-        } else{
-            return this.pedersen_verifier.verifyProof(a, b, c, input);
+    public boolean verify(BigInteger boardId, BigInteger[] a, BigInteger[][] b, BigInteger[] c) {
+        for (int i = 0; i < boards.size(); i++) {
+            Board board = boards.get(i);
+            if (board.getSha256Id().equals(boardId)) {
+                return this.sha256BLS12381Verifier.verifyProof(a, b, c, new BigInteger[]{boardId});
+            }
+            if (board.getPedersenId().equals(boardId)) {
+                return this.pedersenBN128Verifier.verifyProof(a, b, c, new BigInteger[]{boardId});
+            }
         }
+        throw new IllegalArgumentException("Board with id = " + boardId + " does not exist!");
     }
 
 }
